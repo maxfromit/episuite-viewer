ba2094b205c82b5fdfcafa377af5070082b9d685
maxfromit <bigmaxcapacity@gmail.com>
2026-01-16 06:55:49 +0300

    refactor(app): remove footer component

--- app/app.vue
+++ app/app.vue
@@ -7,30 +7,5 @@ useSeoMeta({ title: 'Epi Suite viewer' })
     <UMain>
       <NuxtPage />
     </UMain>
-
-    <UFooter>
-      <template #left>
-        <p class="text-muted text-sm">Maxim Polyakov • © 2026</p>
-      </template>
-
-      <template #right>
-        <UButton
-          to="https://github.com/maxfromit/episuite-viewer"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
-        />
-        <UButton
-          to="https://episuite.dev/EpiWebSuite/#/results;cas=002921-88-2"
-          target="_blank"
-          icon="i-lucide-external-link"
-          aria-label="Epi Suite"
-          color="neutral"
-          variant="ghost"
-        />
-      </template>
-    </UFooter>
   </UApp>
 </template>
