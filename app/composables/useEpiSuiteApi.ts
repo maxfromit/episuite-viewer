aed8e43c207ac199ef3a25b9b4fdffa6c90afacb
maxfromit <bigmaxcapacity@gmail.com>
2026-01-16 05:54:37 +0300

    feat(nuxt): disable SSR

--- app/composables/useEpiSuiteApi.ts
+++ app/composables/useEpiSuiteApi.ts
@@ -4,9 +4,7 @@ export const useEpiSuiteApi = (cas: string) => {
   const { data, pending, error } = useFetch<SubmitResponse>(
     'https://episuite.dev/EpiWebSuite/api/submit',
     {
-      query: { cas },
-      lazy: true,
-      server: false
+      query: { cas }
     }
   )
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+
+  ssr: false,
 
   devtools: {
     enabled: true
